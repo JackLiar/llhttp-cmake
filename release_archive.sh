f8106bfc3cbf28c3437dbc16182d74a10931b891
Jack Liar <15205739+JackLiar@users.noreply.github.com>
2020-03-04 21:40:37 +0800

    update llhttp to 2.0.5

--- release_archive.sh
+++ release_archive.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-cd llhttp && git pull && cd ..
-tar cvzf llhttp-2.0.4.tar.gz cmake llhttp .gitignore CMakeLists.txt llhttp.pc.in README.md
+cd llhttp && git pull origin release && cd ..
+tar cvzf llhttp-2.0.5.tar.gz cmake llhttp .gitignore CMakeLists.txt llhttp.pc.in README.md
